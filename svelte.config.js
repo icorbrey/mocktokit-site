c320dc4fec8b614cd30e0f2772d50134c4940835
Isaac Corbrey <icorbrey@gmail.com>
2021-08-16 16:48:02 -0400

    wip: Add `svelte-hmr` to noExternal list

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,21 @@
 import adapter from '@sveltejs/adapter-netlify'
 import preprocess from 'svelte-preprocess'
 
+const isDev = () => process.env.NODE_ENV === 'development'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
 		target: '#svelte',
 		adapter: adapter(),
+		vite: {
+			ssr: {
+				noExternal: isDev() ? [] : [
+					'svelte-hmr'
+				]
+			}
+		}
 	}
 }
 
